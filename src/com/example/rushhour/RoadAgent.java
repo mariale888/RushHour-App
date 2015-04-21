54b3e2e87353b78b7ad935b5e1890e863d416671
Parag Goel <paraggoel14@users.noreply.github.com>
2015-04-21 18:22:42 -0400

    Create RoadAgent.java

---
+++ src/com/example/rushhour/RoadAgent.java
@@ -0,0 +1,11 @@
+public class RoadAgent
+{
+    int road_ID;
+    int intersection_ID1;
+    int intersection_ID2;
+    int kind_of_road; //Yet to define mapping of type of road to integer
+    double width;
+    int no_of_lanes;
+    double length;
+    double congestion_factor;
+}
